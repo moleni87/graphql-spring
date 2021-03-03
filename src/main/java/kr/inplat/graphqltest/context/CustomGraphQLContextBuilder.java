d07e8de045c243fa07b5302ba9ca35273fa6401d
seo <moleni87@gmail.com>
2021-03-03 18:33:29 +0900

    DataLoader

--- src/main/java/kr/inplat/graphqltest/context/CustomGraphQLContextBuilder.java
+++ src/main/java/kr/inplat/graphqltest/context/CustomGraphQLContextBuilder.java
@@ -10,9 +10,11 @@ import kr.inplat.graphqltest.domain.repository.BookRepository;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.dataloader.DataLoader;
+import org.dataloader.DataLoaderOptions;
 import org.dataloader.DataLoaderRegistry;
 import org.dataloader.MappedBatchLoader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +22,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.websocket.Session;
 import javax.websocket.server.HandshakeRequest;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -28,6 +32,9 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
 public class CustomGraphQLContextBuilder implements GraphQLServletContextBuilder {
 
     Log log = LogFactory.getLog(this.getClass());
+
+    private static final Executor priceExecutor = new DelegatingSecurityContextExecutorService(Executors
+            .newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
 
     @Autowired
     private BookRepository bookRepository;
@@ -53,8 +60,11 @@ public class CustomGraphQLContextBuilder implements GraphQLServletContextBuilder
 
     private DataLoaderRegistry buildDataLoaderRegistry() {
         DataLoaderRegistry dataLoaderRegistry = new DataLoaderRegistry();
-        MappedBatchLoader<Long, List<Book>> bookDataLoader = keys -> supplyAsync(() -> bookRepository.findByAuthorIdIn(keys).stream().collect(Collectors.groupingBy(Book::getAuthorId)));
-        dataLoaderRegistry.register("bookDataLoader", DataLoader.newMappedDataLoader(bookDataLoader));
+        MappedBatchLoader<Long, List<Book>> bookDataLoader = keys -> supplyAsync(
+                () -> bookRepository.findByAuthorIdIn(keys).stream().collect(Collectors.groupingBy(Book::getAuthorId))
+        , priceExecutor);
+        log.info(priceExecutor);
+        dataLoaderRegistry.register("bookDataLoader", DataLoader.newMappedDataLoader(bookDataLoader, DataLoaderOptions.newOptions().setMaxBatchSize(20)));
         return dataLoaderRegistry;
     }
 }
